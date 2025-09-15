bf61c53513aa46c3008218dd5a1a5a0dabff36a3
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-15 15:28:32 +0200

    Improve showing version

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"runtime/debug"
 )
 
 func main() {
+	for _, arg := range os.Args {
+		if arg == "-V" || arg == "--version" {
+			info, _ := debug.ReadBuildInfo()
+			fmt.Println("LBRY CLI " + info.Main.Version)
+			os.Exit(0)
+		}
+	}
+	//TODO
 	os.Exit(0)
 }
