8b77fc485cc8bacde74054a3e104a7dad5f6d315
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-18 01:34:00 +0200

    Implement get command

--- commands/get.go
+++ commands/get.go
@@ -1,15 +1,82 @@
 package commands
 
+import "context"
+import "encoding/json"
+import "fmt"
+import "strconv"
 import "github.com/spf13/cobra"
+import "github.com/ybbus/jsonrpc/v3"
 
 func CreateCommandGet() *cobra.Command {
 	get := &cobra.Command{
 		Use:   "get",
 		Short: "Download stream from a LBRY name.",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandGet,
 	}
 
+	get.Flags().String("uri", "", "(str) uri of the content to download")
+	get.Flags().String("file_name", "", "(str) specified name for the downloaded file, overrides the stream file name")
+	get.Flags().String("download_directory", "", "(str) full path to the directory to download into")
+	get.Flags().Int("timeout", -1, "(int) download timeout in number of seconds")
+	get.Flags().Bool("save_file", false, "(bool) save the file to the downloads directory")
+	get.Flags().String("wallet_id", "", "(str) wallet to check for claim purchase receipts")
+
 	return get
+}
+
+func HandleCommandGet(cmd *cobra.Command, args []string) {
+	uri, _ := cmd.Flags().GetString("uri")
+	file_name, _ := cmd.Flags().GetString("file_name")
+	download_directory, _ := cmd.Flags().GetString("download_directory")
+	timeout, _ := cmd.Flags().GetInt("timeout")
+	save_file, _ := cmd.Flags().GetBool("save_file")
+	wallet_id, _ := cmd.Flags().GetString("wallet_id")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		uri = args[0]
+	}
+	if len(args) >= 2 {
+		file_name = args[1]
+	}
+	if len(args) >= 3 {
+		download_directory = args[2]
+	}
+	if len(args) >= 4 {
+	    val, _ := strconv.Atoi(args[3])
+		timeout = val
+	}
+
+	if uri == "" {
+		cmd.Help()
+	}
+
+	params := map[string]any{
+		"uri": uri,
+	}
+	if file_name != "" {
+		params["file_name"] = file_name
+	}
+	if download_directory != "" {
+		params["download_directory"] = download_directory
+	}
+	if timeout != -1 {
+		params["timeout"] = timeout
+	}
+	if save_file {
+		params["save_file"] = save_file
+	}
+	if wallet_id != "" {
+		params["wallet_id"] = wallet_id
+	}
+	fmt.Println(params)
+
+	rpcClient := jsonrpc.NewClient("http://localhost:5279/")
+	resp, err := rpcClient.Call(context.Background(), "get", params)
+	if err != nil {
+		fmt.Println("Could not connect to daemon. Are you sure it's running?")
+		return
+	}
+	result, _ := json.MarshalIndent(resp.Result, "", "\t")
+	fmt.Println(string(result))
 }
