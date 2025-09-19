9e4904a6e953b0e6bb2d1c58c97c655f3561cdd7
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-19 15:34:55 +0200

    Implement UTXO commands

--- commands/utxo/list.go
+++ commands/utxo/list.go
@@ -1,15 +1,50 @@
 package commands_utxo
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandUTXOList() *cobra.Command {
 	utxo_list := &cobra.Command{
 		Use:   "list",
 		Short: "List unspent transaction outputs",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandUTXOList,
 	}
 
+	utxo_list.Flags().String("account_id", "", "(str) id of the account to query")
+	utxo_list.Flags().String("wallet_id", "", "(str) restrict results to specific wallet")
+	utxo_list.Flags().Int("page", -1, "(int) page to return during paginating")
+	utxo_list.Flags().Int("page_size", -1, "(int) number of items on page during pagination")
+
 	return utxo_list
+}
+
+func HandleCommandUTXOList(cmd *cobra.Command, args []string) {
+	account_id, _ := cmd.Flags().GetString("account_id")
+	wallet_id, _ := cmd.Flags().GetString("wallet_id")
+	page, _ := cmd.Flags().GetInt("page")
+	page_size, _ := cmd.Flags().GetInt("page_size")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		account_id = args[0]
+	}
+
+	params := map[string]any{}
+	if account_id != "" {
+		params["account_id"] = account_id
+	}
+	if wallet_id != "" {
+		params["wallet_id"] = wallet_id
+	}
+	if page != -1 {
+		params["page"] = page
+	}
+	if page_size != -1 {
+		params["page_size"] = page_size
+	}
+
+	rpc.ExecuteRPCCommand("utxo_list", params)
 }
--- commands/utxo/release.go
+++ commands/utxo/release.go
@@ -1,15 +1,42 @@
 package commands_utxo
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandUTXORelease() *cobra.Command {
 	utxo_release := &cobra.Command{
 		Use:   "release",
 		Short: "When spending a UTXO it is locally locked to prevent double spends; occasionally this can result in a UTXO being locked which ultimately did not get spent (failed to broadcast, spend transaction was not accepted by blockchain node, etc). This command releases the lock on all UTXOs in your account.",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandUTXORelease,
 	}
 
+	utxo_release.Flags().String("account_id", "", "(str) id of the account to query")
+	utxo_release.Flags().String("wallet_id", "", "(str) restrict results to specific wallet")
+	utxo_release.Flags().Int("page", -1, "(int) page to return during paginating")
+	utxo_release.Flags().Int("page_size", -1, "(int) number of items on page during pagination")
+
 	return utxo_release
+}
+
+func HandleCommandUTXORelease(cmd *cobra.Command, args []string) {
+	account_id, _ := cmd.Flags().GetString("account_id")
+	wallet_id, _ := cmd.Flags().GetString("wallet_id")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		account_id = args[0]
+	}
+
+	params := map[string]any{}
+	if account_id != "" {
+		params["account_id"] = account_id
+	}
+	if wallet_id != "" {
+		params["wallet_id"] = wallet_id
+	}
+
+	rpc.ExecuteRPCCommand("utxo_list", params)
 }
