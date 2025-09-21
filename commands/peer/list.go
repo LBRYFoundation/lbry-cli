2d4a96c64594c5fd7d4d17c0560f10cec9a6da44
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-22 00:52:02 +0200

    Implement peer commands

--- commands/peer/list.go
+++ commands/peer/list.go
@@ -1,15 +1,45 @@
 package commands_peer
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandPeerList() *cobra.Command {
 	peer_list := &cobra.Command{
 		Use:   "list",
 		Short: "Get peers for blob hash",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandPeerList,
 	}
 
+	peer_list.Flags().String("blob_hash", "", "(str) find available peers for this blob hash")
+	peer_list.Flags().Int("page", -1, "(int) page to return during paginating")
+	peer_list.Flags().Int("page_size", -1, "(int) number of items on page during pagination")
+
 	return peer_list
+}
+
+func HandleCommandPeerList(cmd *cobra.Command, args []string) {
+	// Create parameter map
+	params := map[string]any{}
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "blob_hash")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetInt, "page")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetInt, "page_size")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		_, exists := params["blob_hash"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["blob_hash"] = args[0]
+	}
+	if len(args) > 1 {
+		cmd.Help()
+		return
+	}
+
+	rpc.ExecuteRPCCommand("peer_list", params)
 }
--- commands/peer/ping.go
+++ commands/peer/ping.go
@@ -1,15 +1,63 @@
 package commands_peer
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandPeerPing() *cobra.Command {
 	peer_ping := &cobra.Command{
 		Use:   "ping",
 		Short: "Send a kademlia ping to the specified peer. If address and port are provided the peer is directly pinged, if not provided the peer is located first.",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandPeerPing,
 	}
 
+	peer_ping.Flags().String("node_id", "", "")
+	peer_ping.Flags().String("address", "", "")
+	peer_ping.Flags().Int("port", -1, "")
+
 	return peer_ping
+}
+
+func HandleCommandPeerPing(cmd *cobra.Command, args []string) {
+	// Create parameter map
+	params := map[string]any{}
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "node_id")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "address")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetInt, "port")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		_, exists := params["node_id"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["node_id"] = args[0]
+	}
+	if len(args) >= 2 {
+		_, exists := params["address"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["address"] = args[1]
+	}
+	if len(args) >= 3 {
+		_, exists := params["port"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		val, _ := strconv.Atoi(args[2])
+		params["port"] = val
+	}
+	if len(args) > 3 {
+		cmd.Help()
+		return
+	}
+
+	rpc.ExecuteRPCCommand("peer_ping", params)
 }
