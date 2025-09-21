94f2bc9c84a86c50e5a5417d2c853d5584b3cae1
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-21 23:21:09 +0200

    Implement preference commands

--- commands/preference/get.go
+++ commands/preference/get.go
@@ -1,15 +1,50 @@
 package commands_preference
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandPreferenceGet() *cobra.Command {
 	preference_get := &cobra.Command{
 		Use:   "get",
 		Short: "Get preference value for key or all values if not key is passed in.",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
-		},
+		Run:   HandleCommandPreferenceGet,
 	}
 
+	preference_get.Flags().String("key", "", "(str) key associated with value")
+	preference_get.Flags().String("wallet_id", "", "(str) restrict operation to specific wallet")
+
 	return preference_get
+}
+
+func HandleCommandPreferenceGet(cmd *cobra.Command, args []string) {
+	// Create parameter map
+	params := map[string]any{}
+	//rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "key")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "wallet_id")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		_, exists := params["key"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["key"] = args[0]
+	}
+	if len(args) > 1 {
+		cmd.Help()
+		return
+	}
+
+	// Check for required parameters
+	_, exists := params["key"]
+	if !exists {
+		cmd.Help()
+		return
+	}
+
+	rpc.ExecuteRPCCommand("preference_get", params)
 }
--- commands/preference/set.go
+++ commands/preference/set.go
@@ -1,6 +1,10 @@
 package commands_preference
 
-import "github.com/spf13/cobra"
+import (
+	"lbry/cli/rpc"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandPreferenceSet() *cobra.Command {
 	preference_set := &cobra.Command{
@@ -11,5 +15,53 @@ func CreateCommandPreferenceSet() *cobra.Command {
 		},
 	}
 
+	preference_set.Flags().String("key", "", "(str) key associated with value")
+	preference_set.Flags().String("value", "", "(str) key associated with value")
+	preference_set.Flags().String("wallet_id", "", "(str) restrict operation to specific wallet")
+
 	return preference_set
+}
+
+func HandleCommandPreferenceSet(cmd *cobra.Command, args []string) {
+	// Create parameter map
+	params := map[string]any{}
+	//rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "key")
+	//rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "value")
+	rpc.AddParameter(params, cmd.Flags(), cmd.Flags().GetString, "wallet_id")
+
+	// Check for arguments
+	if len(args) >= 1 {
+		_, exists := params["key"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["key"] = args[0]
+	}
+	if len(args) >= 2 {
+		_, exists := params["value"]
+		if exists {
+			cmd.Help()
+			return
+		}
+		params["value"] = args[1]
+	}
+	if len(args) > 2 {
+		cmd.Help()
+		return
+	}
+
+	// Check for required parameters
+	_, existsKey := params["key"]
+	if !existsKey {
+		cmd.Help()
+		return
+	}
+	_, existsKey := params["value"]
+	if !existsKey {
+		cmd.Help()
+		return
+	}
+
+	rpc.ExecuteRPCCommand("preference_set", params)
 }
