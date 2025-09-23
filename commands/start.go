30bfea093a39fdf51a6ceda1a77c82e19809055c
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-09-23 17:05:23 +0200

    Fix start command

--- commands/start.go
+++ commands/start.go
@@ -1,12 +1,29 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 func CreateCommandStart() *cobra.Command {
 	start := &cobra.Command{
 		Use:   "start",
 		Short: "Start LBRY Network interface.",
+		Run:   HandleCommandStart,
 	}
 
 	return start
+}
+
+func HandleCommandStart(cmd *cobra.Command, args []string) {
+	// Check for arguments
+	if len(args) > 0 {
+		cmd.Help()
+		return
+	}
+
+	fmt.Println("The daemon cannot be started using the CLI.")
+	os.Exit(1)
 }
