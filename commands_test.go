515d1e7e4946fd70c388d3bc28feaea78dc31e02
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-10-05 01:59:17 +0200

    Improve test

--- commands_test.go
+++ commands_test.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/spf13/cobra"
 )
+
+func captureStdout(f func()) string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	f() // run the function that writes to stdout
+
+	_ = w.Close()
+	var buf bytes.Buffer
+	_, _ = io.Copy(&buf, r)
+	os.Stdout = old
+
+	return buf.String()
+}
 
 func TestCreateCommand(t *testing.T) {
 	cmd := CreateCommand()
@@ -15,7 +34,24 @@ func TestCreateCommand(t *testing.T) {
 }
 
 func TestHandleCommand(t *testing.T) {
+	helpCalled := false
+
 	cmd := &cobra.Command{}
+	cmd.Flags().Bool("version", false, "")
+	cmd.SetHelpFunc(func(*cobra.Command, []string) {
+		helpCalled = true
+	})
 
 	HandleCommand(cmd, nil)
+	if !helpCalled {
+		t.Error("Help should have been called if --version is absent")
+	}
+
+	cmd.Flags().Set("version", "true")
+	output := captureStdout(func() {
+		HandleCommand(cmd, nil)
+	})
+	if !strings.HasPrefix(output, "LBRY CLI") {
+		t.Error("Version should start with 'LBRY CLI'.")
+	}
 }
